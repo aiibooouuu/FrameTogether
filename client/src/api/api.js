18969465e9e0bc067448d921e9be408a49069781
Hamza <2005abuhamza@gmail.com>
2026-07-03 21:14:58 +0530

    minor udpates adne env changes and deployments hit v1

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -5,6 +5,10 @@
  * ==========================================================
  */
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
+console.log("API URL =", API_BASE_URL);
 
 export default API_BASE_URL;
